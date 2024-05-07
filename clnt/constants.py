b5b6308d911ffc465d357bc9d366d5a7e19a185a
EdanJ <109800775+EdanJacobson@users.noreply.github.com>
2024-05-07 21:55:48 +0300

    Add files via upload

---
+++ clnt/constants.py
@@ -0,0 +1,34 @@
+"""
+Edan Jacobson
+Constants for client classes
+"""
+
+CLIENT_IP = "192.168.86.36"
+PORT = 8080
+
+MSG_LEN = 1024
+READ_SIZE = 4
+BYTE_SIZE = 4
+CHUNK_SIZE = 1024
+
+FILE_NAME = -1
+REQUEST = 0
+PATH = 0
+SECOND_PATH = 1
+
+ONE_PARAM = 1
+TWO_PARAMS = 2
+NO_PARAMS = 0
+
+RECEIVED_FILE_LOCATION = "c:\\test_folder\\client\\"
+EOF = b'-1'
+
+EXIT = 1
+
+NO_DATA = 0
+FIRST = 0
+CHARS = 128
+
+RANGE_POS_1 = 5
+RANGE_POS_2 = -1
+RANGE_POS_3 = -1
